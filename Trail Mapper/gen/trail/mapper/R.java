ea5c6c7922e2615e5289b30a4326158ac68e0f12
Jeremy Tobias <tobias.vfx@gmail.com>
2012-10-28 20:41:08 -0400

    Moved some things around so the Main Activity isn't so crowded. Created a new Intent/Activity (GetLocation)
    Signed-off-by: Jeremy Tobias <tobias.vfx@gmail.com>

--- Trail Mapper/gen/trail/mapper/R.java
+++ Trail Mapper/gen/trail/mapper/R.java
@@ -10,32 +10,43 @@ package trail.mapper;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int padding_large=0x7f040002;
+        public static final int padding_medium=0x7f040001;
+        public static final int padding_small=0x7f040000;
+    }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int address=0x7f060004;
-        public static final int findMe=0x7f060000;
-        public static final int label_address=0x7f060003;
-        public static final int label_latlng=0x7f060001;
-        public static final int latlng=0x7f060002;
+        public static final int address=0x7f070003;
+        public static final int findMe=0x7f070004;
+        public static final int label_address=0x7f070002;
+        public static final int label_latlng=0x7f070000;
+        public static final int latlng=0x7f070001;
+        public static final int menu_settings=0x7f070005;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int activity_get_location=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f050000;
+        public static final int activity_get_location=0x7f060000;
+        public static final int main=0x7f060001;
     }
     public static final class string {
-        public static final int address=0x7f040001;
-        public static final int app_name=0x7f040000;
-        public static final int enable_gps=0x7f040002;
-        public static final int enable_gps_dialog=0x7f040003;
-        public static final int find_me=0x7f040008;
-        public static final int latlng=0x7f040004;
-        public static final int not_support_gps=0x7f040006;
-        public static final int not_support_network=0x7f040007;
-        public static final int unknown=0x7f040005;
+        public static final int address=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int enable_gps=0x7f050002;
+        public static final int enable_gps_dialog=0x7f050003;
+        public static final int find_me=0x7f050008;
+        public static final int hello_world=0x7f050009;
+        public static final int latlng=0x7f050004;
+        public static final int menu_settings=0x7f05000a;
+        public static final int not_support_gps=0x7f050006;
+        public static final int not_support_network=0x7f050007;
+        public static final int title_activity_get_location=0x7f05000b;
+        public static final int unknown=0x7f050005;
     }
 }
